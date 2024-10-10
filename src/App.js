fce09df5e3b20b1d48ca581735c270976ff1e814
CodeWithNaveed <getinfo.naveed@gmail.com>
2024-10-10 22:05:34 +0500

    ADD: detail page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import Detail from './components/Detail';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
+      <Router>
         <Header />
-        <Home />
+
+        <Routes>
+          <Route path="/detail" element={<Detail />} />
+
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
